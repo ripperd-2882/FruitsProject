77324d07149dd11d7b20da01bffbcc6cb5061374
ripperd-2882 <ripperd2882@gmail.com>
2023-04-17 23:32:50 +0530

    created db with mongoose to insert data

--- app.js
+++ app.js
@@ -1,57 +1,19 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://localhost:27017/fruitsDB");
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
+const fruitSchema=new mongoose.Schema({
+    name:String,
+    rating:Number,
+    review:String
+});
 
+const Fruit=mongoose.model("Fruit",fruitSchema);
 
+const fruit =new Fruit({
+    name:"Apple",
+    rating:7,
+    review:"Good"
+});
 
-
-
-
-
-
-
-
-
-const { MongoClient } = require("mongodb");
-
-// Replace the uri string with your connection string.
-const uri = "mongodb://127.0.0.1:27017";
-
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-        const database = client.db('fruitsDB');
-        console.log("Connected successfully to server");
-
-        // Query for a movie that has the title 'Back to the Future'
-        const collection = database.collection('fruits');
-        const query = [{
-            name: "Apple",
-            score: 8,
-            review: "Great Fruit"
-        },
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kinda Sour"
-        },
-        {
-            name: "Apple",
-            score: 9,
-            review: "Great Stuff"
-        }]
-
-        // const fruits2 = await collection.insertMany(query);
-        const fruits3 = collection.find();
-        await fruits3.forEach(console.dir);
-        // console.log(fruits3);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
-
+fruit.save();
