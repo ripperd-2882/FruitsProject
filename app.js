630bd7a1d46b59ac402578d5aa550a4dcf8ee03d
ripperd-2882 <ripperd2882@gmail.com>
2023-04-18 00:25:09 +0530

    Added many fruits

--- app.js
+++ app.js
@@ -2,33 +2,67 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
-const fruitSchema=new mongoose.Schema({
-    name:String,
-    rating:Number,
-    review:String
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
 });
 
-const Fruit=mongoose.model("Fruit",fruitSchema);
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit =new Fruit({
-    name:"Apple",
-    rating:7,
-    review:"Good"
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Good"
 });
 
 // fruit.save();
 
-
-const personSchema=new mongoose.Schema({
-    name:String,
-    age:Number
+const kiwi = new Fruit({
+    name: "Kiwi",
+    rating: 10,
+    review: "Best"
 });
 
-const Person=mongoose.model("Person",personSchema);
-
-const person =new Person({
-    name:"John",
-    age:37
+const orange = new Fruit({
+    name: "Orange",
+    rating: 4,
+    review: "Sour"
 });
 
-person.save();
+const banana = new Fruit({
+    name: "Banana",
+    rating: 3,
+    review: "Wierd"
+});
+
+// Fruit.insertMany([kiwi,orange,banana]).then (function (err) {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log("Success");    
+// });
+
+Fruit.insertMany([kiwi, orange, banana]).then(function () {
+    console.log("Successfully saved all the fruits to fruitsDB.");
+}).catch(function (err) {
+    console.log(err);
+});
+
+
+
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John",
+    age: 37
+});
+
+// person.save();
