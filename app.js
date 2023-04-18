d959b1ee573f8baf5a156f14d58bd52e3ce5ad4c
ripperd-2882 <ripperd2882@gmail.com>
2023-04-18 13:05:45 +0530

    Added validators

--- app.js
+++ app.js
@@ -3,45 +3,72 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type:String,
+        required:true
+    },
+    rating: {
+        type:Number,
+        min:1,
+        max:10
+    },
     review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-    name: "Apple",
-    rating: 7,
+    // name: "Apple",
+    rating: 27,
     review: "Good"
 });
 
-// fruit.save();
+fruit.save();
 
-const kiwi = new Fruit({
-    name: "Kiwi",
-    rating: 10,
-    review: "Best"
-});
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     rating: 10,
+//     review: "Best"
+// });
 
-const orange = new Fruit({
-    name: "Orange",
-    rating: 4,
-    review: "Sour"
-});
+// const orange = new Fruit({
+//     name: "Orange",
+//     rating: 4,
+//     review: "Sour"
+// });
 
-const banana = new Fruit({
-    name: "Banana",
-    rating: 3,
-    review: "Wierd"
-});
+// const banana = new Fruit({
+//     name: "Banana",
+//     rating: 3,
+//     review: "Wierd"
+// });
 
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+//     From Course
 // // // Fruit.insertMany([kiwi,orange,banana]).then (function (err) {
 // // //     if (err)
 // // //         console.log(err);
 // // //     else
 // // //         console.log("Success");    
 // // // });
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 // Fruit.insertMany([kiwi, orange, banana]).then(function () {
 //     console.log("Successfully saved all the fruits to fruitsDB.");
